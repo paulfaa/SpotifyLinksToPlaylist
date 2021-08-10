fe11316190fa0af57eb8e8e3b912907c28fb0514
Paul <paulfarrell12@gmail.com>
2021-08-10 20:47:14 +0100

    implemented spotipy

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import os
 import re
 from tkinter import filedialog, Text
 import logging
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
+def authenticate():
+    try:
+        clientId = open(os.path.join(os.getcwd)+'client_id', 'r')
+        clientSecret = open(os.path.join(os.getcwd)+'client_secret', 'r')
+        sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientId, client_secret=clientSecret))
+    except:
+        logging.exception('Failed to authenticate')
 
 def selectFile():
     try:
@@ -15,19 +24,22 @@ def selectFile():
 
 
 def convert():
-    folderPath = ''
-    fileToConvert = selectFile()
+    fileToParse = selectFile()
     urls = []
     try:
-        with open(fileToConvert, 'r', encoding='utf8') as textFile:
+        with open(fileToParse, 'r', encoding='utf8') as textFile:
             for line in textFile:
                 if re.search(r"""https://open.spotify.com/track/""", line):
                     words = line.split()
                     for word in words:
                         if re.search(r"""^(https)""", word):
-                            urls.append(word[31:53])
-                            print("Found match")
-        print(urls)
+                            urls.append('spotify:track:' + word[31:53])
+        if len(words) >= 1:
+            print(urls)
+            return words
+        else:
+            logging.error('No Track IDs found')
+            return None
     except:
         logging.exception('Read file failed')
 
