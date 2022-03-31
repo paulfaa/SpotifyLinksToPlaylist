e88e41f8be21d1f47510d3c46f5d6d8d40e05d72
Paul <paulfarrell12@gmail.com>
2022-03-31 15:10:15 +0100

    Refactor to remove duplicate ids

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ import logging
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+playlist_id = "playlist_id_goes_here"
+getUrl = "/v1/playlists/{playlist_id}/tracks"
+postUrl = "	/v1/playlists/{playlist_id}/tracks"
+sp
+
 def authenticate():
     try:
         clientId = open(os.path.join(os.getcwd)+'client_id', 'r')
         clientSecret = open(os.path.join(os.getcwd)+'client_secret', 'r')
+        global sp
         sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientId, client_secret=clientSecret))
     except:
         logging.exception('Failed to authenticate')
@@ -22,10 +28,20 @@ def selectFile():
     except:
         logging.exception('Failed to select file')
 
+def getExistingUris():
+    try:
+        return sp.playlist_tracks(playlist_id)
+    except:
+        logging.exception('Get request failed')
 
-def convert():
-    fileToParse = selectFile()
-    urls = []
+def updatePlaylist(newUris):
+    try:
+        sp.playlist_add_items(playlist_id, newUris, "end")
+    except:
+        logging.exception('Post request failed')
+
+def findNewUris(fileToParse, existingUris):
+    newUris = []
     try:
         with open(fileToParse, 'r', encoding='utf8') as textFile:
             for line in textFile:
@@ -33,14 +49,23 @@ def convert():
                     words = line.split()
                     for word in words:
                         if re.search(r"""^(https)""", word):
-                            urls.append('spotify:track:' + word[31:53])
-        if len(words) >= 1:
-            print(urls)
-            return words
-        else:
-            logging.error('No Track IDs found')
-            return None
+                            uri = 'spotify:track:' + word[31:53]
+                            if (uri not in existingUris):
+                                newUris.append(uri)
     except:
         logging.exception('Read file failed')
 
-convert()
+def run():
+    authenticate()
+    fileToParse = selectFile()
+    existingUris = getExistingUris()
+    newUris = findNewUris(fileToParse, existingUris)
+    if len(newUris) >= 1:
+        print(newUris)
+        updatePlaylist(newUris)
+    else:
+        logging.error('No new Track IDs found')
+        return None
+
+run()
+ 
